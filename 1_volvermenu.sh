0a90b312ee59b17632b403d35a4205ccbf914795
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-03-01 15:09:44 +0100

    Update and rename volver to 1_volvermenu.sh

--- 1_volvermenu.sh
+++ 1_volvermenu.sh
@@ -2,7 +2,7 @@
 
 
 
-volvermenu(){
+function volvermenu(){
     read -p "Introduzca Q o q para salir y m o M para volver al menu: " letra  #se guaradara lo que pongas en letra 
     case "$letra" in
         m|M)																	#si se cumple una de estas 2 directivas te devolvera al menu principal
