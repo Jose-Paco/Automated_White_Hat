406177c119237e65f56d781a41ee0535dfd45bc9
GuilleFdezz <99341585+GuilleFdezz@users.noreply.github.com>
2022-03-03 15:41:01 +0100

    Update 1_vectores.sh
    añado -q

--- 1_vectores.sh
+++ 1_vectores.sh
@@ -1,14 +1,14 @@
  #!/bin/bash
 function vectores(){ 
-   login= $(wget $1 | grep "login")
-   coment= $(wget $1 | grep "comment")
-   filter= $(wget $1 | grep "filter") 
-   search= $(wget $1 | grep "search")
-   category= $(wget $1 | grep "comment")
-   password= $(wget $1 | grep "password")
-   username= $(wget $1 | grep "username")
-   img= $(wget $1 | grep "img")
-   product= $(wget $1 | grep "product") 
+   login= $(wget -q $1 | grep "login")
+   coment= $(wget -q $1 | grep "comment")
+   filter= $(wget -q $1 | grep "filter") 
+   search= $(wget -q $1 | grep "search")
+   category= $(wget -q $1 | grep "comment")
+   password= $(wget -q $1 | grep "password")
+   username= $(wget -q $1 | grep "username")
+   img= $(wget -q $1 | grep "img")
+   product= $(wget -q $1 | grep "product") 
    if [ -e $login ] || [ -e $password ] || [ -e $username ]
       diccionario=true
    if [ -e $filter ] || [ -e $category ] || [ -e $product ]
