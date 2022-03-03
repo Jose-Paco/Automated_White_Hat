85db90b38f270bc8bdd2891d149a02a632718a43
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-03-03 16:43:24 +0100

    Update 2_instalaciones.sh

--- 2_instalaciones.sh
+++ 2_instalaciones.sh
@@ -18,24 +18,23 @@ function instalaciones(){
 
 	            sudo apt-get update	> /dev/null 2>&1				#te hara una actualizacion 
 
-            dpkg -s unzip &> /dev/null  			#revisa si se encuentra istalado
+            dpkg -s unzip > /dev/null 2>&1 			#revisa si se encuentra istalado
 
 	    if [ $? -ne 0 ]								#si esta dara 1 asi que te dira que esta instalado y no hara nada 
 	
 	        then
 	            echo "UNZIP not installed, installing..."  
-	            sudo apt-get install unzip > /dev/null 2>&1	#te instalara el paquete
+	            sudo apt-get install unzip -y	> /dev/null 2>&1	#te instalara el paquete
 	
 	        else
 	            echo    "UNZIP Installed"
 	    fi
 	    	    	        	dpkg -s curl > /dev/null 2>&1  			#revisa si se encuentra istalado
-
 	    if [ $? -ne 0 ]								#si esta dara 1 asi que te dira que esta instalado y no hara nada 
 	
 	        then
 	            echo "curl not installed, installing..."  
-	            sudo apt-get install curl > /dev/null 2>&1	#te instalara el paquete
+	            sudo apt-get install curl -y > /dev/null 2>&1	#te instalara el paquete
 	
 	        else
 	            echo    "curl Installed"
@@ -46,7 +45,7 @@ function instalaciones(){
 	
 	        then
 	            echo "gnupg not installed, installing..."  
-	            sudo apt-get install gnupg > /dev/null 2>&1	#te instalara el paquete
+	            sudo apt-get install gnupg -y > /dev/null 2>&1	#te instalara el paquete
 	
 	        else
 	            echo    "gnupg Installed"
@@ -54,66 +53,64 @@ function instalaciones(){
 	    	    	    	        	dpkg -s lsb-release > /dev/null 2>&1  			#revisa si se encuentra istalado
 
 	    if [ $? -ne 0 ]								#si esta dara 1 asi que te dira que esta instalado y no hara nada 
-	
+	    	
 	        then
 	            echo "lsb-release not installed, installing..."  
-	            sudo apt-get install lsb-release > /dev/null 2>&1	#te instalara el paquete
-	
-	        else
+	            sudo apt-get install lsb-release -y	> /dev/null 2>&1	#te instalara el paquete
+        else
 	            echo    "lsb-release Installed"
 	    fi
-	    	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" &
-	 	echo \
-  	"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  	$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-      
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &
+         sudo apt-get update	> /dev/null 2>&1
 	    	    	    	    	        	dpkg -s ca-certificates > /dev/null 2>&1 			#revisa si se encuentra istalado
 
 	    if [ $? -ne 0 ]								#si esta dara 1 asi que te dira que esta instalado y no hara nada 
 	
 	        then
 	            echo "ca-certificates not installed, installing..."  
-	            sudo apt-get install ca-certificates > /dev/null 2>&1		#te instalara el paquete
+	            sudo apt-get install ca-certificates -y > /dev/null 2>&1		#te instalara el paquete
 	
 	        else
 	            echo    "ca-certificates Installed"
 	    fi
 	    	    	    	        	dpkg -s containerd.io > /dev/null 2>&1 			#revisa si se encuentra istalado
 
+	    
 	    if [ $? -ne 0 ]								#si esta dara 1 asi que te dira que esta instalado y no hara nada 
 	
 	        then
 	            echo "containerd.io not installed, installing..."  
-	            sudo apt-get install containerd.io > /dev/null 2>&1	#te instalara el paquete
+	            sudo apt-get install containerd.io -y > /dev/null 2>&1	#te instalara el paquete
 	
 	        else
 	            echo    "containerd.io Installed"
 	    fi
 	    
-	        dpkg -s docker-ce &> /dev/null  			#revisa si se encuentra istalado
+	        dpkg -s docker-ce > /dev/null 2>&1 			#revisa si se encuentra istalado
 
 	    if [ $? -ne 0 ]								#si esta dara 1 asi que te dira que esta instalado y no hara nada 
 	
 	        then
 	            echo "Docker not installed, installing..."   
-	            sudo apt-get install docker-ce > /dev/null 2>&1	#te instalara el paquete
+	            sudo apt-get install docker-ce -y > /dev/null 2>&1	#te instalara el paquete
 	
 	        else
-	            echo    "Docker Installed"
-	    fi
-	    dpkg -s docker-ce-cli &> /dev/null  			#revisa si se encuentra istalado
+echo "Docker Installed"
+
+    fi
+	    dpkg -s docker-ce-cli > /dev/null 2>&1  			#revisa si se encuentra istalado
 
 	    if [ $? -ne 0 ]								#si esta dara 1 asi que te dira que esta instalado y no hara nada 
 	
 	        then
 	            echo "Docker utilities not installed, installing..."  
-	            sudo apt-get install docker-ce-cli > /dev/null 2>&1	#te instalara el paquete
-	
+	            sudo apt-get install docker-ce-cli -y > /dev/null 2>&1	#te instalara el paquete
+		    
+
 	        else
 	            echo    "Docker utilities Installed"
-	    fi
+fi
 }
