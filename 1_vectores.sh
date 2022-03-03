9c54b94838b75acfdedcf059b8832e72a82f4e06
GuilleFdezz <99341585+GuilleFdezz@users.noreply.github.com>
2022-03-03 16:00:16 +0100

    Update 1_vectores.sh

--- 1_vectores.sh
+++ 1_vectores.sh
@@ -4,16 +4,21 @@ function vectores(){
    coment= $(wget -q -O - $1 | grep "comment")
    filter= $(wget -q -O -  $1 | grep "filter") 
    search= $(wget -q -O - $1 | grep "search")
-   category= $(wget -q $1 | grep "comment")
+   category= $(wget -q -O - $1 | grep "comment")
    password= $(wget -q -O - $1 | grep "password")
    username= $(wget -q -O - $1 | grep "username")
    img= $(wget -q -O -  $1 | grep "img")
    product= $(wget -q -O - $1 | grep "product") 
    if [ -e $login ] || [ -e $password ] || [ -e $username ]
       diccionario=true
+   fi
    if [ -e $filter ] || [ -e $category ] || [ -e $product ]
       sql=true
+   fi
    if [ -e $comment ] || [ -e $search ] || [ -e $img ] 
       xss=true
+   fi
+   declare -a vectores=('$xss' '$sql' '$diccionario')
+   return $vectores
 }
 #podemos meter mas patrones al filtro
