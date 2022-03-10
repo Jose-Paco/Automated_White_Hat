0f7e1be1547f51d2662aba657e11ec846dbe5b98
GuilleFdezz <99341585+GuilleFdezz@users.noreply.github.com>
2022-03-10 15:09:11 +0100

    Update montarswarm.sh

--- montarswarm.sh
+++ montarswarm.sh
@@ -11,9 +11,5 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
-ip4=$(ip a | head -n9 | tail -1 | sed '1s!/! !g' | awk '{print $2}')
-sudo docker swarm init --advertise-addr $ip4
-sudo docker pull debian
 sudo docker pull owasp/zap2docker-stable
-sudo docker run debian
 sudo docker run owasp/zap2docker-stable
