27d971600fde5adaae5578d2e6295330ee5ef076
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-03-10 15:48:09 +0100

    Update 1_vectores.sh

--- 1_vectores.sh
+++ 1_vectores.sh
@@ -4,27 +4,27 @@ function vectores(){
    xss=false
    diccionario=false
    sql=false
-   login= $(wget -q -O - $1 | grep "login")
-   coment= $(wget -q -O - $1 | grep "comment")
-   filter= $(wget -q -O -  $1 | grep "filter") 
-   search= $(wget -q -O - $1 | grep "search")
-   category= $(wget -q -O - $1 | grep "comment")
-   password= $(wget -q -O - $1 | grep "password")
-   username= $(wget -q -O - $1 | grep "username")
-   img= $(wget -q -O -  $1 | grep "img")
-   product= $(wget -q -O - $1 | grep "product") 
-   if [ -e $login ] || [ -e $password ] || [ -e $username ]
-      diccionario=true
+   login=$(wget -q -O - "$1" | grep "login")
+   coment=$(wget -q -O - "$1" | grep "comment")
+   filter=$(wget -q -O -  "$1" | grep "filter") 
+   search=$(wget -q -O - "$1" | grep "search")
+   category=$(wget -q -O - "$1" | grep "comment")
+   password=$(wget -q -O - "$1" | grep "password")
+   username=$(wget -q -O - "$1" | grep "username")
+   img=$(wget -q -O -  "$1" | grep "img")
+   product=$(wget -q -O - "$1" | grep "product") 
+   if [ "$login" ] || [ "$password" ] || [ "$username" ]
+   then
       vectores+=("diccionario")
    fi
-   if [ -e $filter ] || [ -e $category ] || [ -e $product ]
-      sql=true
+   if [ "$filter" ] || [ "$category" ] || [ "$product" ]
+   then
       vectores+=("sql")
    fi
-   if [ -e $comment ] || [ -e $search ] || [ -e $img ] 
-      xss=true
+   if [ "$coment" ] || [ "$search" ] || [ "$img" ]
+   then
       vectores+=("xss")
    fi
-   return $vectores()
+   return $vectores
 }
 #podemos meter mas patrones al filtro
