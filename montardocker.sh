c8291382475cb2558e5325db264700528a86cf27
GuilleFdezz <99341585+GuilleFdezz@users.noreply.github.com>
2022-03-28 16:49:37 +0200

    Update and rename montarswarm.sh to montardocker.sh

--- montardocker.sh
+++ montardocker.sh
@@ -11,6 +11,8 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+svn checkout https://github.com/stefpe/squid_proxy.git
+sudo docker network create --subnet=172.20.0.0/24 customnetwork
 sudo docker pull owasp/zap2docker-stable
 sudo docker run -i owasp/zap2docker-stable zap-cli quick-scan --self-contained     --start-options '-config api.disablekey=true' https://acac1f0f1fb13e1fc1393f69001b00b8.web-security-academy.net
 #ejemplo que levanta el docker
