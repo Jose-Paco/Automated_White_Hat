c44e626333a959b2edae1ca92bc15696fd8dd9a7
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-04-26 16:29:33 +0200

    Update 1_volvermenu.sh

--- 1_volvermenu.sh
+++ 1_volvermenu.sh
@@ -2,12 +2,12 @@
 
 
 
-function volvermenu(){
+function 3_volvermenu(){
     read -p "Introduzca Q o q para salir y m o M para volver al menu: " letra  #se guaradara lo que pongas en letra 
     case "$letra" in
         m|M)																	#si se cumple una de estas 2 directivas te devolvera al menu principal
 			clear
-            '_menuPrincipal'
+            '3_menuPrincipal'
             ;;
         q|Q)																	#si se cumple una de estas 2 directivas te sacara del programa
 			clear
@@ -16,7 +16,7 @@ function volvermenu(){
         *)  
             echo "No has introducido m ni q"									#si marcas cualquier cosa te dara error y te devovlera al mismo menu hasta que pongas una de las 2 condiciones que hay
             clear
-            'volvermenu'
+            '3_volvermenu'
             ;;
     esac
 }
