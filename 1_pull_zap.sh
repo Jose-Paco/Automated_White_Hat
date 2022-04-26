73bcc999bc25a1d0b5f549a46552107f22d4f229
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-04-26 16:30:11 +0200

    Delete 1_pull_zap.sh

--- 1_pull_zap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-function pull_zap(){
-#  sudo docker pull debian
-  sudo docker pull owasp/zap2docker-stable
-#  sudo docker run debian
-  sudo docker run owasp/zap2docker-stable
-}
