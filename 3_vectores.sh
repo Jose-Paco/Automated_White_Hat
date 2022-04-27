98e4d58bd9457931f35fd2b981f7b68f84d20f35
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-04-27 15:58:50 +0200

    Update 3_vectores.sh

--- 3_vectores.sh
+++ 3_vectores.sh
@@ -3,13 +3,41 @@ function 3_vectores(){
    declare -a vectores=()
    csrf=$(wget -q -O - "$1" | grep "csrf")
    login=$(wget -q -O - "$1" | grep "login")
-   coment=$(wget -q -O - "$1" | grep "comment")
+#   comment=$(wget -q -O - "$1" | grep "comment")
    filter=$(wget -q -O -  "$1" | grep "filter") 
    search=$(wget -q -O - "$1" | grep "search")
    category=$(wget -q -O - "$1" | grep "category")
    password=$(wget -q -O - "$1" | grep "password")
    username=$(wget -q -O - "$1" | grep "username")
-   product=$(wget -q -O - "$1" | grep "product") 
+   product=$(wget -q -O - "$1" | grep "product")
+   if [[ wget -q -O - "$1" | grep "product" ]]  || [[ $url grep "product" ]]
+   then
+      product="a"
+   fi
+   if [[ wget -q -O - "$1" | grep "password" ]]  || [[ $url grep "password" ]]
+   then
+      password="a"
+   fi
+   if [[ wget -q -O - "$1" | grep "username" ]]  || [[ $url grep "username" ]]
+   then
+      username="a"
+   fi
+      if [[ wget -q -O - "$1" | grep "login" ]]  || [[ $url grep "login" ]]
+   then
+      login="a"
+   fi
+      if [[ wget -q -O - "$1" | grep "category" ]]  || [[ $url grep "category" ]]
+   then
+      category="a"
+   fi
+   if [[ wget -q -O - "$1" | grep "search" ]]  || [[ $url grep "search" ]]
+   then
+      search="a"
+   fi
+      if [[ wget -q -O - "$1" | grep "filter" ]]  || [[ $url grep "filter" ]]
+   then
+      filter="a"
+   fi
    if [ "$csrf" ]
    then
       vectores=("csrf")
