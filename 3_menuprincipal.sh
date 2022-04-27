4b6a0650ed426a965797f414073d5be3195c0408
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-04-27 16:51:41 +0200

    Update 3_menuprincipal.sh

--- 3_menuprincipal.sh
+++ 3_menuprincipal.sh
@@ -40,11 +40,9 @@ function 3_menuPrincipal() {
 			esac
 		fi
 	else
-		if [[ ${vector[@]} | grep 'diccionario' ]]
+		if [[ ${vector[@]} | grep 'sql' ]]
 		then
-			if [[ ${vector[@]} | grep 'sql' ]]
-			
-			echo -e "Para ejecutar un ataque de Diccionario pulsa 1.\nPara ejecutar un ataque de XSS pulsa 3."
+			echo -e "Para ejecutar un ataque de SqlInjection pulsa 1.\nPara ejecutar un ataque de XSS pulsa 3."
      			read -p "" tipoDeAtaque
 			3_instalaciones
 			case $tipoDeAtaque in
@@ -57,11 +55,8 @@ function 3_menuPrincipal() {
 					3_xss.sh
 					;;
 			esac
-	fi
-    else
-    	if [[ ${vector[@]} | grep 'sql' ]]
-	then
-			echo -e "Para ejecutar un ataque de SqlInjection pulsa 2.\nPara ejecutar un ataque de XSS pulsa 3."
+		else
+			echo -e "Para ejecutar un ataque de XSS pulsa 3."
      			read -p "" tipoDeAtaque
 			3_instalaciones
 			case $tipoDeAtaque in
@@ -69,6 +64,17 @@ function 3_menuPrincipal() {
 					clear
 					3_xss.sh
 					;;
+			else
+		fi
+	fi
+    else
+    	if [[ ${vector[@]} | grep 'sql' ]]
+	then
+		if [[ ${vector[@]} | grep 'diccionario' ]]
+			echo -e "Para ejecutar un ataque de SqlInjection pulsa 2.\nPara ejecutar un ataque de Diccionario pulsa 3."
+     			read -p "" tipoDeAtaque
+			3_instalaciones
+			case $tipoDeAtaque in
 				2)
 					clear
 					3_sqli.sh
@@ -78,24 +84,35 @@ function 3_menuPrincipal() {
 					3_xss.sh
 					;;
 			esac
+		else
+			echo -e "Para ejecutar un ataque de SqlInjection pulsa 2"
+     			read -p "" tipoDeAtaque
+			3_instalaciones
+			case $tipoDeAtaque in
+				2)
+					clear
+					3_sqli.sh
+					;;
 	else
-		echo -e "Para ejecutar un ataque de Diccionario pulsa 1.\nPara ejecutar un ataque de SqlInjection pulsa 2.\nPara ejecutar un ataque de XSS pulsa 3."
-     		read -p "" tipoDeAtaque
-		3_instalaciones
-		case $tipoDeAtaque in
-			1)
-				clear
-				3_xss.sh
-				;;
-			2)
-				clear
-				3_sqli.sh
-				;;
-			3)
-				clear
-				3_xss.sh
-				;;
+		if [[ ${vector[@]} | grep 'diccionario' ]]
+			echo -e "Para ejecutar un ataque de Diccionario pulsa 1.\n"
+	     		read -p "" tipoDeAtaque
+			3_instalaciones
+			case $tipoDeAtaque in
+				1)
+					clear
+					3_xss.sh
+					;;
+
 		esac
+		else
+			if [[ ${vector[@]} | grep 'csrf' ]]
+			then
+				echo -e "La web esta protegida contra los ataques que analizamos."
+				exit 0
+			fi
+			echo -e "No es vulnerable a nada la web."
+		fi
 	fi
     fi
     
