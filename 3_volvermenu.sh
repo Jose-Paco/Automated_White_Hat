105b0f2b833b03df0a4b2066c3f4d6334d978d92
GuilleFdezz <99341585+GuilleFdezz@users.noreply.github.com>
2022-04-28 15:31:04 +0200

    sin ()

--- 3_volvermenu.sh
+++ 3_volvermenu.sh
@@ -1,8 +1,5 @@
 #/bin/bash
-
-
-
-function 3_volvermenu(){
+function 3_volvermenu {
     read -p "Introduzca Q o q para salir y m o M para volver al menu: " letra  #se guaradara lo que pongas en letra 
     case "$letra" in
         m|M)																	#si se cumple una de estas 2 directivas te devolvera al menu principal
