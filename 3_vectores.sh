4825462356fda0d9bf2b208c46c54b70589aa886
GuilleFdezz <99341585+GuilleFdezz@users.noreply.github.com>
2022-04-28 16:41:28 +0200

    Update 3_vectores.sh

--- 3_vectores.sh
+++ 3_vectores.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
 vectores() {
-   vectores=( "mesa" "silla" )
-   csrf=$(wget -q -O - "$1" | grep "csrf")
-   login=$(wget -q -O - "$1" | grep "login")
-#   comment=$(wget -q -O - "$1" | grep "comment")
-   filter=$(wget -q -O -  "$1" | grep "filter") 
-   search=$(wget -q -O - "$1" | grep "search")
-   category=$(wget -q -O - "$1" | grep "category")
-   password=$(wget -q -O - "$1" | grep "password")
-   username=$(wget -q -O - "$1" | grep "username")
-   product=$(wget -q -O - "$1" | grep "product")
-   if [[ wget -q -O - "$1" | grep "product" ]]  || [[ $url grep "product" ]]
+   csrf=$(wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "csrf")
+   login=$(wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "login")
+#   comment=$(wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "comment")
+   filter=$(wget -q -O -  "https://diccionarivalencia.com/?paraula=a" | grep "filter") 
+   search=$(wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "search")
+   category=$(wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "category")
+   password=$(wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "password")
+   username=$(wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "username")
+   product=$(wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "product")
+   if [[ wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "product" ]]  || [[ $url grep "product" ]]
    then
       product="a"
    fi
-   if [[ wget -q -O - "$1" | grep "password" ]]  || [[ $url grep "password" ]]
+   if [[ wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "password" ]]  || [[ $url grep "password" ]]
    then
       password="a"
    fi
-   if [[ wget -q -O - "$1" | grep "username" ]]  || [[ $url grep "username" ]]
+   if [[ wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "username" ]]  || [[ $url grep "username" ]]
    then
       username="a"
    fi
-      if [[ wget -q -O - "$1" | grep "login" ]]  || [[ $url grep "login" ]]
+      if [[ wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "login" ]]  || [[ $url grep "login" ]]
    then
       login="a"
    fi
-      if [[ wget -q -O - "$1" | grep "category" ]]  || [[ $url grep "category" ]]
+      if [[ wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "category" ]]  || [[ $url grep "category" ]]
    then
       category="a"
    fi
-   if [[ wget -q -O - "$1" | grep "search" ]]  || [[ $url grep "search" ]]
+   if [[ wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "search" ]]  || [[ $url grep "search" ]]
    then
       search="a"
    fi
-      if [[ wget -q -O - "$1" | grep "filter" ]]  || [[ $url grep "filter" ]]
+      if [[ wget -q -O - "https://diccionarivalencia.com/?paraula=a" | grep "filter" ]]  || [[ $url grep "filter" ]]
    then
       filter="a"
    fi
    if [ "$csrf" ]
    then
-      vectores=("csrf")
+      vectores="csrf"
    else
     if [ "$login" ] || [ "$password" ] || [ "$username" ]
     then
-       vectores+=("diccionario")
+       diccionario="diccionario"
     fi
     if [ "$filter" ] || [ "$category" ] || [ "$product" ]
     then
-       vectores+=("sql")
+       sql="sql"
     fi
     if [ "$coment" ] || [ "$search" ]
     then
-       vectores+=("xss")
+       xss="xss"
     fi
    fi
+   vectores = $sql$xss$diccionario
    return $vectores
 }
