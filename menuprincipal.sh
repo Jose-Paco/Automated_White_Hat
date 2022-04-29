809382fe00f12533e76399ce22dea8e3ff929d9b
GuilleFdezz <99341585+GuilleFdezz@users.noreply.github.com>
2022-04-29 15:41:32 +0200

    Update menuprincipal.sh

--- menuprincipal.sh
+++ menuprincipal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source controlerror.sh
+#source controlerror.sh
 source volvermenu.sh
 source instalaciones.sh
 source diccionario.sh
@@ -21,7 +21,7 @@ function menuPrincipal() {
     then
     	echo -e "Para ejecutar un ataque de Diccionario pulsa 1.\nPara ejecutar un ataque de SqlInjection pulsa 2.\nPara ejecutar un ataque de XSS pulsa 3."
      	read -p "" tipoDeAtaque
-	instalaciones
+	'instalaciones'
 	case $tipoDeAtaque in
 		1)
 			clear
@@ -35,101 +35,91 @@ function menuPrincipal() {
 			clear
 			'xss'
 			;;
+		*)
 	esac
-     else
-	if [[ echo $vector | grep "xss" && grep "diccionario" ]]
-    	then
-    		echo -e "Para ejecutar un ataque de Diccionario pulsa 1.\nPara ejecutar un ataque de XSS pulsa 2."
-	     	read -p "" tipoDeAtaque
-		instalaciones
-		case $tipoDeAtaque in
-			1)
-				clear
-				'diccionario'
-				;;
-			2)
-				clear
-				'xss'
-				;;
-		esac
-      	else
-		if [[ echo $vector | grep "sql" && grep "diccionario" ]]
-		then
-		    	echo -e "Para ejecutar un ataque de Diccionario pulsa 1.\nPara ejecutar un ataque de SQL pulsa 2."
-		     	read -p "" tipoDeAtaque
-			instalaciones
-			case $tipoDeAtaque in
-				1)
-					clear
-					'diccionario'
-					;;
-				2)
-					clear
-					'sqli'
-					;;
-			esac
-      		else
-			if [[ echo $vector | grep "diccionario" ]]
-		    	then
-			    	echo -e "Para ejecutar un ataque de Diccionario pulsa 1."
-			     	read -p "" tipoDeAtaque
-				instalaciones
-				case $tipoDeAtaque in
-					1)
-						clear
-						'diccionario'
-						;;
-				esac
-      			else
-				if [[ echo $vector | grep "xss" && grep "sql" ]]
-				then
-				    	echo -e "Para ejecutar un ataque de SQL pulsa 1.\nPara ejecutar un ataque de XSS pulsa 2."
-				     	read -p "" tipoDeAtaque
-					instalaciones
-					case $tipoDeAtaque in
-						1)
-							clear
-							'sqli'
-							;;
-						2)
-							clear
-							'xss'
-							;;
-					esac
-				else
-					if [[ echo $vector | grep "sql" ]]
-					then
-					        echo -e "Para ejecutar un ataque de SqlInjection pulsa 1.\n"
-     						read -p "" tipoDeAtaque
-						instalaciones
-						case $tipoDeAtaque in
-							1)
-								clear
-								'sqli'
-								;;
-						esac
-					else
-						if [[ echo $vector | grep "xss" ]]
-						then
-						    	echo -e "Para ejecutar un ataque de XSS pulsa 1."
-						     	read -p "" tipoDeAtaque
-							instalaciones
-							case $tipoDeAtaque in
-								1)
-									clear
-									'xss'
-									;;
-							esac
-						fi
-					fi
-				fi
-			fi
-		fi
+    elif [[ echo $vector | grep "xss" && grep "diccionario" ]]
+    then
+    	echo -e "Para ejecutar un ataque de Diccionario pulsa 1.\nPara ejecutar un ataque de XSS pulsa 2."
+	     read -p "" tipoDeAtaque
+	'instalaciones'
+	case $tipoDeAtaque in
+		1)
+			clear
+			'diccionario'
+			;;
+		2)
+			clear
+			'xss'
+			;;
+		*)
+	esac
+     elif [[ echo $vector | grep "sql" && grep "diccionario" ]]
+     then
+	echo -e "Para ejecutar un ataque de Diccionario pulsa 1.\nPara ejecutar un ataque de SQL pulsa 2."
+	read -p "" tipoDeAtaque
+	'instalaciones'
+	case $tipoDeAtaque in
+		1)
+			clear
+			'diccionario'
+			;;
+		2)
+			clear
+			'sqli'
+			;;
+		*)
+	esac
+     elif [[ echo $vector | grep "diccionario" ]]
+     then
+	    echo -e "Para ejecutar un ataque de Diccionario pulsa 1."
+	    read -p "" tipoDeAtaque
+	    'instalaciones'
+	    case $tipoDeAtaque in
+		1)
+			clear
+			'diccionario'
+			;;
+		*)
+	esac
+     elif [[ echo $vector | grep "xss" && grep "sql" ]]
+     then
+     	echo -e "Para ejecutar un ataque de SQL pulsa 1.\nPara ejecutar un ataque de XSS pulsa 2."
+	read -p "" tipoDeAtaque
+	'instalaciones'
+	case $tipoDeAtaque in
+		1)
+			clear
+			'sqli'
+			;;
+		2)
+			clear
+			'xss'
+			;;
+		*)
+	esac 
+      elif [[ echo $vector | grep "sql" ]]
+      then
+      	echo -e "Para ejecutar un ataque de SqlInjection pulsa 1.\n"
+     	read -p "" tipoDeAtaque
+	'instalaciones'
+	case $tipoDeAtaque in
+		1)
+			clear
+			'sqli'
+			;;
+		*)
+	esac
+      elif [[ echo $vector | grep "xss" ]]
+      then
+        echo -e "Para ejecutar un ataque de XSS pulsa 1."
+     	read -p "" tipoDeAtaque
+	'instalaciones'
+	case $tipoDeAtaque in
+		1)
+			clear
+			'xss'
+			;;
+		*)
+	esac
 	fi
-    #de aqui en adelante todo mal
-    
-    while [$vector > 3 | $vector < 1]
-    do 
-        echo -e "Para ejecutar un ataque de Diccionario pulsa 1.\nPara ejecutar un ataque de SqlInjection pulsa 2.\nPara ejecutar un ataque de XSS pulsa 3."
-        read -p "" tipoDeAtaque
 }
