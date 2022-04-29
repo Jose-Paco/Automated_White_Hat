39c62b97eff31da14c4d93e24bd64f13b924e04a
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-04-29 16:05:28 +0200

    Update checkurl.sh

--- checkurl.sh
+++ checkurl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 checkurl() {
+#este script pide una url al usuario y si no dispone de una url vuelve a llamar a la funcion de pedir una url, si todo va bien, cierra la funcion y devuelve como valor un string con la url.
 read -p "introduce una url: " url
 if [ "$url" != "" ]
 then
@@ -14,5 +15,3 @@ then
     fi
 fi
 }
-#main
-#'checkurl'
