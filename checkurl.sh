6fb97cae590596a625884d641d1b74b86196f546
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-05-03 15:35:00 +0200

    Update checkurl.sh

--- checkurl.sh
+++ checkurl.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 checkurl() {
 #este script pide una url al usuario y si no dispone de una url vuelve a llamar a la funcion de pedir una url, si todo va bien, cierra la funcion y devuelve como valor un string con la url.
-read -p "introduce una url: " url
+read -p "Introduce una url: " url
 if [ "$url" != "" ]
 then
-    wget $url &> /dev/null
-    while [ $? != "0" ]
-    do
-      'checkurl' 
-    done
-    if [ "$?" == "0" ]
+    if [ curl -i -s $url | grep "200 OK" ]
     then
         return $url
+    else
+        echo -e "La url $url no es valida"
     fi
+else
+    echo -e "No has introducido ninguna URL"
+    'checkurl'
 fi
 }
