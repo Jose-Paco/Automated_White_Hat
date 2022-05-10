f13c3431549283a0843c344933333a14840d7e63
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-05-10 17:47:50 +0200

    Update volvermenu.sh

--- volvermenu.sh
+++ volvermenu.sh
@@ -1,19 +1,15 @@
-#/bin/bash
 volvermenu() {
 #esta funcion sirve para ser llamada cuando se acaba un ataque
-    read -p "Introduzca Q o q para salir y m o M para volver al menu: " letra  #se guaradara lo que pongas en letra 
+    read -r -p "Introduzca Q o q para salir y m o M para volver al menu: " letra  #se guaradara lo que pongas en letra 
     case "$letra" in
         m|M)																	#si se cumple una de estas 2 directivas te devolvera al menu principal
-			clear
-            'menuPrincipal'
+            'menuprincipal'
             ;;
         q|Q)																	#si se cumple una de estas 2 directivas te sacara del programa
-			clear
             exit
             ;;
         *)  
             echo "No has introducido m ni q"									#si marcas cualquier cosa te dara error y te devovlera al mismo menu hasta que pongas una de las 2 condiciones que hay
-            clear
 	    'volvermenu'
             ;;
     esac
